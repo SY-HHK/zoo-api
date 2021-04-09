3d657a608547908303cc383dacc244dc39da3270
SY-HHK <louis.ricour@gmail.com>
2021-04-09 16:55:46 +0200

    add admin user crud routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,8 @@
 import {Express} from "express";
 import {authRouter} from "./auth.router";
+import {userRouter} from "./admin/user.router";
 
 export function buildRoutes(app: Express) {
     app.use("/auth", authRouter);
+    app.use("/admin/user", userRouter);
 }
