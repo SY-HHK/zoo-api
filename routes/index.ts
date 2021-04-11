5941498ff4af68dde517695e7707572d61f020b4
SY-HHK <louis.ricour@gmail.com>
2021-04-11 12:31:38 +0200

    add admin/worker routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import {Express} from "express";
 import {authRouter} from "./auth.router";
 import {userRouter} from "./admin/user.router";
+import {workerRouter} from "./admin/worker.router.js";
 
 export function buildRoutes(app: Express) {
     app.use("/auth", authRouter);
     app.use("/admin/user", userRouter);
+    app.use("/admin/worker", workerRouter);
 }
