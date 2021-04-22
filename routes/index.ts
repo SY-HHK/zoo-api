2f391950311f5cd6d01a714e13937ae916ce6bbe
SY-HHK <louis.ricour@gmail.com>
2021-04-22 11:48:15 +0200

    feat: add ticketType admin crud routes to router

--- routes/index.ts
+++ routes/index.ts
@@ -3,10 +3,12 @@ import {authRouter} from "./auth.router";
 import {userRouter} from "./admin/user.router";
 import {workerRouter} from "./admin/worker.router";
 import {roleRouter} from "./admin/role.router";
+import {ticketTypeRouter} from "./admin/ticketType.router"
 
 export function buildRoutes(app: Express) {
     app.use("/auth", authRouter);
     app.use("/admin/user", userRouter);
     app.use("/admin/worker", workerRouter);
     app.use("/admin/role", roleRouter);
+    app.use("/admin/ticket-type", ticketTypeRouter);
 }
