f954c48e41f7816934226c5b8e288e835027dd0a
SY-HHK <louis.ricour@gmail.com>
2021-04-23 15:10:19 +0200

    feat: code animal crud methods

--- controllers/animal.controller.ts
+++ controllers/animal.controller.ts
@@ -23,19 +23,56 @@ export class AnimalController {
         this.Area = Area;
     }
 
-    public async create(props: AnimalCreationProps): Promise<AnimalInstance | null> {
-
+    public async create(props: AnimalCreationProps, areaId: number): Promise<AnimalInstance | null> {
+        const area: AreaInstance | null = await this.Area.findOne({
+            where: {
+                id: areaId
+            }
+        });
+        if (area === null) {
+            return null;
+        }
+        const animal: AnimalInstance | null = await this.Animal.create({
+            ...props
+        });
+        if (animal === null) {
+            return null;
+        }
+        await animal.setArea(area);
+        return animal;
     }
 
     public async read(id: number): Promise<AnimalInstance | null> {
-
+        return await this.Animal.findOne({
+            where: {
+                id
+            }
+        });
     }
 
-    public async update(id: number, props: AnimalCreationProps): Promise<AnimalInstance | null> {
-
+    public async update(id: number, props: AnimalCreationProps, areaId: number): Promise<AnimalInstance | null> {
+        const animal: AnimalInstance | null = await this.read(id);
+        const area: AreaInstance | null = await this.Area.findOne({
+            where: {
+                id: areaId
+            }
+        });
+        if (animal === null || area === null) {
+            return null;
+        }
+        animal.name = props.name;
+        animal.species = props.species;
+        animal.journal = props.journal;
+        await animal.setArea(area);
+        return animal;
     }
 
     public async delete(id: number): Promise<boolean> {
-
+        const animal: AnimalInstance | null = await this.read(id);
+        if (animal === null) {
+            return false;
+        }
+        await animal.destroy();
+        return true;
     }
 }
