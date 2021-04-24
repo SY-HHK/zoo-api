9c09252b47fcaff0c35741e86d9d841c588d2776
Louis <louis.ricour@gmail.com>
2021-04-24 18:14:29 +0200

    feat: code image crud routes

--- routes/admin/image.router.ts
+++ routes/admin/image.router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import {adminMiddleware} from "../../middlewares/auth.middleware";
 import {checkIdMiddleware} from "../../middlewares/utils.middleware";
+import {ImageController} from "../../controllers/image.controller";
+import {ImageInstance} from "../../models/image.model";
 
 const imageRouter = express.Router();
 
@@ -9,7 +11,20 @@ const imageRouter = express.Router();
  * @return image
  */
 imageRouter.post("/create", adminMiddleware, async function(req, res) {
-
+    const base64: string = req.body.base64;
+    const areaId: number = req.body.areaId !== undefined ? parseInt(req.body.areaId, 10) : 0;
+    if (base64 === undefined || areaId === 0) {
+        res.status(400).end();
+        return;
+    }
+    const imageController: ImageController = await ImageController.getInstance();
+    const image: ImageInstance | null = await imageController.create({path:''}, base64, areaId);
+    if (image === null) {
+        res.status(409).end();
+        return;
+    }
+    const imageData: string | Buffer = await require('fs/promises').readFile(image.path);
+    res.status(201).send(imageData).end();
 });
 
 /**
@@ -17,7 +32,15 @@ imageRouter.post("/create", adminMiddleware, async function(req, res) {
  * @return image
  */
 imageRouter.get("/get/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const imageController: ImageController = await ImageController.getInstance();
+    const image: ImageInstance | null = await imageController.read(id);
+    if (image === null) {
+        res.status(404).end();
+        return;
+    }
+    const imageData: string | Buffer = await require('fs/promises').readFile(image.path);
+    res.status(200).send(imageData).end();
 });
 
 /**
@@ -25,7 +48,20 @@ imageRouter.get("/get/:id", adminMiddleware, checkIdMiddleware, async function(r
  * @return image
  */
 imageRouter.put("/update/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const areaId: number = req.body.areaId !== undefined ? parseInt(req.body.areaId, 10) : 0;
+    if (areaId === 0) {
+        res.status(400).end();
+        return;
+    }
+    const imageController: ImageController = await ImageController.getInstance();
+    const image: ImageInstance | null = await imageController.update(id, areaId);
+    if (image === null) {
+        res.status(404).end();
+        return;
+    }
+    const imageData: string | Buffer = await require('fs/promises').readFile(image.path);
+    res.status(200).send(imageData).end();
 });
 
 /**
@@ -33,7 +69,14 @@ imageRouter.put("/update/:id", adminMiddleware, checkIdMiddleware, async functio
  * @return void
  */
 imageRouter.delete("/delete/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const imageController: ImageController = await ImageController.getInstance();
+    const isDeleted: boolean = await imageController.delete(id);
+    if (!isDeleted) {
+        res.status(404).end();
+        return;
+    }
+    res.status(200).end();
 });
 
 export {
