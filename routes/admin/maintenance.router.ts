cbc43c09d052d0d35995be130d2f81b07a17e44a
Louis <louis.ricour@gmail.com>
2021-04-24 20:03:39 +0200

    feat: code maintenance crud routes

--- routes/admin/maintenance.router.ts
+++ routes/admin/maintenance.router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import {adminMiddleware} from "../../middlewares/auth.middleware";
 import {checkIdMiddleware} from "../../middlewares/utils.middleware";
+import {MaintenanceController} from "../../controllers/maintenance.controller";
+import {MaintenanceInstance} from "../../models/maintenance.model";
 
 const maintenanceRouter = express.Router();
 
@@ -9,7 +11,22 @@ const maintenanceRouter = express.Router();
  * @return maintenance
  */
 maintenanceRouter.post("/create", adminMiddleware, async function(req, res) {
-
+    const start: string = req.body.startDate;
+    const end: string = req.body.endDate;
+    const areaId: number = req.body.areaId !== undefined ? parseInt(req.body.areaId, 10) : 0;
+    if (start === undefined || end === undefined || areaId === 0) {
+        res.status(400).end();
+        return;
+    }
+    const startDate: Date = new Date(start);
+    const endDate: Date = new Date(end);
+    const maintenanceController: MaintenanceController = await MaintenanceController.getInstance();
+    const maintenance: MaintenanceInstance | null = await maintenanceController.create({startDate, endDate}, areaId);
+    if (maintenance === null) {
+        res.status(409).end();
+        return;
+    }
+    res.status(201).json(maintenance).end();
 });
 
 /**
@@ -17,7 +34,14 @@ maintenanceRouter.post("/create", adminMiddleware, async function(req, res) {
  * @return animal
  */
 maintenanceRouter.get("/get/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const maintenanceController: MaintenanceController = await MaintenanceController.getInstance();
+    const maintenance: MaintenanceInstance | null = await maintenanceController.read(id);
+    if (maintenance === null) {
+        res.status(404).end();
+        return;
+    }
+    res.status(200).json(maintenance).end();
 });
 
 /**
@@ -25,7 +49,23 @@ maintenanceRouter.get("/get/:id", adminMiddleware, checkIdMiddleware, async func
  * @return maintenance
  */
 maintenanceRouter.put("/update/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const start: string = req.body.startDate;
+    const end: string = req.body.endDate;
+    const areaId: number = req.body.areaId !== undefined ? parseInt(req.body.areaId, 10) : 0;
+    if (start === undefined || end === undefined || areaId === 0) {
+        res.status(400).end();
+        return;
+    }
+    const startDate: Date = new Date(start);
+    const endDate: Date = new Date(end);
+    const maintenanceController: MaintenanceController = await MaintenanceController.getInstance();
+    const maintenance: MaintenanceInstance | null = await maintenanceController.update(id, {startDate, endDate}, areaId);
+    if (maintenance === null) {
+        res.status(404).end();
+        return;
+    }
+    res.status(200).json(maintenance).end();
 });
 
 /**
@@ -33,7 +73,14 @@ maintenanceRouter.put("/update/:id", adminMiddleware, checkIdMiddleware, async f
  * @return void
  */
 maintenanceRouter.delete("/delete/:id", adminMiddleware, checkIdMiddleware, async function(req, res) {
-
+    const id: number = parseInt(req.params.id, 10);
+    const maintenanceController: MaintenanceController = await MaintenanceController.getInstance();
+    const isDeleted: boolean = await maintenanceController.delete(id);
+    if (isDeleted) {
+        res.status(404).end();
+        return;
+    }
+    res.status(200).end();
 });
 
 export {
