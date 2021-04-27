25f12f56f0862b075f2261c7c375518e9b888845
SY-HHK <louis.ricour@gmail.com>
2021-04-27 16:24:38 +0200

    fix: import new areType router file

--- routes/index.ts
+++ routes/index.ts
@@ -11,7 +11,7 @@ import {maintenanceRouter} from "./admin/maintenance.router";
 import {visitRouter} from "./visit.router";
 import {adminRouter} from "./admin/admin.router";
 import {areaRouter} from "./admin/area.router";
-import {areaTypeRouter} from "./admin/areatype.router";
+import {areaTypeRouter} from "./admin/areaType.router";
 
 export function buildRoutes(app: Express) {
     app.use("/auth", authRouter);
